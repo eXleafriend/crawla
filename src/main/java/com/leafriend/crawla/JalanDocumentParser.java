f94fd8bfc149e8041eefab3ac9d62ca060f42530
leafriend <leafriend@gmail.com>
2016-04-26 21:55:23 +0900

    Implement Jalan document parser.

--- src/main/java/com/leafriend/crawla/JalanDocumentParser.java
+++ src/main/java/com/leafriend/crawla/JalanDocumentParser.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class JalanDocumentParser implements DocumentParser {
 
@@ -11,6 +13,22 @@ public class JalanDocumentParser implements DocumentParser {
     public List<Comment> parseDocument(Document document) {
 
         List<Comment> comments = new ArrayList<>();
+
+        Elements items = document.getElementsByClass("kuchikomi-list-cassette");
+        for (Element item : items) {
+
+            Element userName = item.getElementsByClass("user-name").get(0);
+            String[] frags = userName.childNodes().get(4).toString().split("/");
+            Gender gender = Gender.parse(frags[0].trim());
+            int age = DocumentParser.parseAge(frags[1].trim());
+
+            Element contents = userName.parent().nextElementSibling();
+
+            String subject = contents.child(0).text();
+            String body = contents.childNode(5).toString();
+            Comment comment = new Comment(age, gender, subject, body);
+            comments.add(comment);
+        }
 
         return comments;
 
