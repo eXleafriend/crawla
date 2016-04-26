47db2f9a9c02f98e4e67d86a8de21a10e4589571
leafriend <leafriend@gmail.com>
2016-04-26 21:04:43 +0900

    Let parsing method return comments

--- src/main/java/com/leafriend/crawla/DocumentParser.java
+++ src/main/java/com/leafriend/crawla/DocumentParser.java
@@ -1,9 +1,11 @@
 package com.leafriend.crawla;
 
+import java.util.List;
+
 import org.jsoup.nodes.Document;
 
 public interface DocumentParser {
 
-    public void parseDocument(Document document);
+    public List<AnonymousComment> parseDocument(Document document);
 
 }
