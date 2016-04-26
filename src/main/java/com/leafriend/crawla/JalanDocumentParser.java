9775899a34cbaf705cd5118cce9b1912dbc1a66c
leafriend <leafriend@gmail.com>
2016-04-26 21:16:26 +0900

    Add Jalan document parser

---
+++ src/main/java/com/leafriend/crawla/JalanDocumentParser.java
@@ -0,0 +1,19 @@
+package com.leafriend.crawla;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.nodes.Document;
+
+public class JalanDocumentParser implements DocumentParser {
+
+    @Override
+    public List<AnonymousComment> parseDocument(Document document) {
+
+        List<AnonymousComment> comments = new ArrayList<>();
+
+        return comments;
+
+    }
+
+}
