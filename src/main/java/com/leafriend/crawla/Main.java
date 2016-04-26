5be69e294b4bfa781c963c43dda63509f4f5fffb
leafriend <leafriend@gmail.com>
2016-04-26 21:14:19 +0900

    Add main class

---
+++ src/main/java/com/leafriend/crawla/Main.java
@@ -0,0 +1,24 @@
+package com.leafriend.crawla;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class Main {
+
+    private static ClassLoader classLoader = Main.class.getClassLoader();
+
+    public static List<AnonymousComment> parse(DocumentParser parser,
+            String baseUri, String file) throws IOException {
+
+        InputStream in = classLoader.getResourceAsStream(file);
+        Document document = Jsoup.parse(in, "UTF-8", baseUri);
+        List<AnonymousComment> comments = parser.parseDocument(document);
+        return comments;
+
+    }
+
+}
